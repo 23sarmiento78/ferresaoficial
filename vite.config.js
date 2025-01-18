88b45f853efe0f6187accf502a8386fca3b528a7
lksarmiento67 <lksarmiento67@gmail.com>
2025-01-18 18:33:43 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,15 @@
- 
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: '/index.html',
+        productos: '/productos.html'
+      }
+    }
+  }
+}); 
